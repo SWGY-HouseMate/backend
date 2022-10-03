bcf3695a942966d3cd33851787a10469bceea1d8
ksh030506 <llmm030506@naver.com>
2022-10-03 09:20:39 +0900

    controller 정리

--- src/main/java/com/swygbro/housemate/controller/EtcController.java
+++ src/main/java/com/swygbro/housemate/controller/EtcController.java
@@ -4,14 +4,18 @@ import com.swygbro.housemate.login.message.MemberInfo;
 import com.swygbro.housemate.util.member.CurrentMemberUtil;
 import com.swygbro.housemate.util.response.domain.SingleResult;
 import com.swygbro.housemate.util.response.service.ResponseService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("Util 관련 API 입니다.")
 @RequestMapping("/util")
 @RequiredArgsConstructor
 public class EtcController {
@@ -22,6 +26,8 @@ public class EtcController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("현재 로그인된 사용자를 가져옵니다.")
     @GetMapping("/login-member")
     public SingleResult<MemberInfo> currentMember() {
         return responseService.getSingleResult(currentMemberUtil.getCurrentMemberInfoObject());
--- src/main/java/com/swygbro/housemate/controller/HouseWorkController.java
+++ src/main/java/com/swygbro/housemate/controller/HouseWorkController.java
@@ -4,14 +4,17 @@ import com.swygbro.housemate.housework.message.*;
 import com.swygbro.housemate.housework.service.*;
 import com.swygbro.housemate.util.response.domain.SingleResult;
 import com.swygbro.housemate.util.response.service.ResponseService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
 @RestController
+@Api("HouseWork 관련 API 입니다.")
 @RequestMapping("/house-work")
 @RequiredArgsConstructor
 public class HouseWorkController {
@@ -25,6 +28,8 @@ public class HouseWorkController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("집안일을 생성합니다.")
     @PostMapping("/create")
     public SingleResult<HoseWorkCreate> create(@RequestBody CreateHouseWork createHouseWork) throws ParseException {
         return responseService.getSingleResult(houseWorkCreateProcess.execute(createHouseWork));
@@ -33,6 +38,8 @@ public class HouseWorkController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("startAt, EndAt을 기준으로 집안일을 검색합니다. (그룹 멤버 별)")
     @PostMapping("/search-date")
     public SingleResult<HouseWorkByMember> searchDateByGroupORMy(@RequestBody SearchHouseWorkAtDate searchHouseWorkAtDate) {
         return responseService.getSingleResult(searchDateProcess.executeByGroup(searchHouseWorkAtDate));
@@ -41,6 +48,8 @@ public class HouseWorkController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("startAt, EndAt을 기준으로 집안일의 갯수를 검색[한일/안한일]합니다. (그룹 멤버 별)")
     @PostMapping("/count")
     public SingleResult<HouseWorkCountForGroup> houseWorkCountForGroup(@RequestBody SearchHouseWorkAtDate searchHouseWorkAtDate) {
         return responseService.getSingleResult(houseWorkCountProcess.executeByGroup(searchHouseWorkAtDate));
@@ -49,6 +58,8 @@ public class HouseWorkController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("집안일을 완료하거나, 롤백합니다.")
     @PostMapping("/complete")
     public SingleResult<String> updateCompleted(@RequestBody HouseWorkCompleted houseWorkCompleted) {
         return responseService.getSingleResult(houseWorkUtil.completion(houseWorkCompleted.getHouse_work_id(), houseWorkCompleted.getIsCompleted()));
@@ -57,6 +68,8 @@ public class HouseWorkController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
     })
+    @ResponseBody
+    @ApiOperation("반복 주기를 삭제합니다. -> 삭제 할 경우 반복 주기에 관련된 집안일이 모두 삭제됩니다.")
     @DeleteMapping("cycle/{cycle_id}")
     public SingleResult<String> deleteCycle(@PathVariable String cycle_id) {
         return responseService.getSingleResult(cycleUtil.delete(cycle_id));
